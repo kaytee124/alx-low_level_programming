fe38959d9b29ae1e04804b882457ab5ef82549a6
Kwame <ktproductions124@gmail.com>
2023-03-20 22:16:23 -0700

    First C Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,9 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of char: %d byte(s)", sizeof(char));
-	printf("Size of int: %d byte(s)", sizeof(int));
-	printf("Size of long int: %d byte(s)", sizeof(long int));
-	printf("Size of long long: %d byte(s)", sizeof(long long int));
-	printf("Size of float: %d byte(s)", sizeof(float));
+	printf("Size of char: %lu byte(s)\n", sizeof(char));
+	printf("Size of int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of long long: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %lu byte(s)\n", sizeof(float));
 	return (0);
+}
