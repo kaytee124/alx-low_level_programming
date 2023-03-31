0b25cdb5944bd6a4a1ef71edbaf9441c5abed75b
Kwame <ktproductions124@gmail.com>
2023-03-31 12:12:20 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,25 +13,41 @@ void print_times_table(int n)
 
 	if (n >= 0 && n <= 15)
 		for (i = 0; i <= n; i++)
+		{
 			for (a = 0; a <= n; a++)
+			{
 				result = i * a;
 				printf("%d", result);
 				check = i * (a + 1);
 				if (result != (n * n))
-					if (a < n && result < 10 && !(check >= 1)0)
+				{
+					if (a < n && result < 10 && !(check >= 10))
+					{
 						printf(",");
 						printf("   ");
+					}
 					else if (a < n && result < 10 && check >= 10)
+					{
 						printf(",");
 						printf("  ");
+					}
 					else if (a < n && result >= 10 && result < 100 && !(check >= 100))
+					{
 						printf(",");
 						printf("  ");
+					}
 					else if (a < n && result >= 10 && result < 100 && check >= 100)
+					{
 						printf(",");
 						printf(" ");
+					}
 					else if (a < n && result >= 100)
+					{
 						printf(",");
 						printf(" ");
+					}
+				}
+			}
 			printf("\n");
+		}
 }
--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	int i == 0;
+	int i = 0;
 	long j = 1, k = 2;
 
 	while (i < 50)
@@ -26,7 +26,7 @@ int main(void)
 			k += j;
 			j = k - j;
 			printf(", %ld", k);
-			i++
+			i++;
 		}
 	}
 	printf("\n");
