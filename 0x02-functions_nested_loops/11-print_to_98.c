434d2ab477042755b683f9f4ce599c7086906c10
Kwame <ktproductions124@gmail.com>
2023-03-31 07:45:12 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,7 +14,7 @@ void print_to_98(int n)
 		while (n <= 98)
 		{
 			printf("%d", n);
-			if(n < 98)
+			if (n < 98)
 			{
 				printf(",");
 				printf(" ");
@@ -33,8 +33,7 @@ void print_to_98(int n)
 				printf(" ");
 			}
 			n--;
-		}
-	
+		}	
 	}
 	printf("\n");
 }
