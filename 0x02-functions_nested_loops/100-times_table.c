b9b89848efe67ef413b8c1eb78f8086095b71313
Kwame <ktproductions124@gmail.com>
2023-03-31 07:57:50 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 
 	if (n > 0 || n < 15)
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (a = 0; a < n; a++)
+			for (a = 0; a <= n; a++)
 			{
 				result = i * a;
 				printf("%d", result);
